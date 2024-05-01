a8985d59f9f461d63addb59e5f7d97e6575a968f
Tarun Jaikishan <tarunjaikishanpk@gmail.com>
2024-05-01 11:55:32 +0530

    testing cors

--- server/index.js
+++ server/index.js
@@ -19,7 +19,8 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://adya-tarun.netlify.app/"],
+    origin: "https://adya-tarun.netlify.app",
+    // origin: "http://localhost:5173",
     credentials: true,
   })
 );
