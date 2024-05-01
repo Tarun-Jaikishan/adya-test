df9789c921575e3714965da4d4b6aef18046bccd
Tarun Jaikishan <tarunjaikishanpk@gmail.com>
2024-05-01 11:32:50 +0530

    origins changed

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const app = express();
 
 // Middlewares
 app.use(express.json());
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 app.use(cookieParser());
 app.use(
   expSession({
